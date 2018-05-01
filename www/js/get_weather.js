b060d3b470bdeedde5f8b82853930cadea879449
Fangxinzonghuofan <32363032+Fangxinzonghuofan@users.noreply.github.com>
2018-05-01 13:19:10 +0100

    1

--- www/js/get_weather.js
+++ www/js/get_weather.js
@@ -7,6 +7,8 @@ $(document).on("pagecreate","#pagesix",function(){
    var city= $('#textinput').val(); 
       console.log("#searchButton val " + city  );
     getCityWeather(city);
+    checkConnection();
+    
   });            
 });            
 
@@ -120,3 +122,21 @@ function getWeatherForWOEID(woeid, tableName) {
     xmlhttp.send();
     
  }
+
+
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+ 
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+ 
+    alert('Connection type: ' + states[networkState]);
+}
